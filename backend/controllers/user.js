38563b0bbab00e3e2777fde6b33d1d78dca43800
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-29 17:16:31 +0200

    controllers/user.js: correction code d'erreur pour passwd incorrect.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,6 +16,7 @@ const jwt = require("jsonwebtoken");
 //modele users
 const User = require("../models/User");
 
+//--------------------------------------------------------------------------
 // ft signup pour enregistrement de nouveaux utilisateurs
 // avec hash du mot de passe
 
@@ -53,7 +54,7 @@ exports.login = (req, res, next) => {
         .compare(req.body.password, user.password)
         .then((valid) => {
           if (!valid) {
-            return res.status(402).json({ error: "Mot de passe incorrect" });
+            return res.status(401).json({ error: "Mot de passe incorrect" });
           }
           res.status(200).json({
             userId: user._id,
@@ -69,7 +70,7 @@ exports.login = (req, res, next) => {
             ),
           });
         })
-        .catch((error) => res.status(501).json({ error })); //500 = error serveur
+        .catch((error) => res.status(500).json({ error })); //500 = error serveur
     })
-    .catch((error) => res.status(502).json({ error })); //500 = error serveur
+    .catch((error) => res.status(500).json({ error })); //500 = error serveur
 };
