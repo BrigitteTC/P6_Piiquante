479761c94cbebea60decf2030a24c1386026e6f9
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-30 16:27:25 +0200

    Ajout controllers/sauce.js

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,108 @@
+//Piiquante
+
+/*----------------------------------------
+controllers/sauce.js
+creation: 30/03/2022
+auteur BTC
+
+Controllers pour gérer les sauces
+POST, PUT, GET et DELETE
+-------------------------------------------
+*/
+
+const Sauce = require("../models/Sauce");
+const fs = require("fs"); //acces à la gestion des fichiers de Node
+
+//-----------------------------------------------------------------------------------
+
+// Creation d'un objet = POST
+
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id; //on supprime l'ID du sauce enregistré
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`, // Url de l'image: protocole, nom du host: = server et Url de l'image
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+//--------------------------------------------------------------
+
+//Récupération 1 objet = GET
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id,
+  })
+    .then((sauce) => {
+      res.status(200).json(sauce);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
+};
+
+//--------------------------------------------------------------
+
+//Modification d'un objet = PUT
+// 2 cas:
+//    1: L'utilisateur modifie les infos
+//    2: l'utilisateur modifie l'image = nouvelle image à traiter..
+exports.modifysauce = (req, res, next) => {
+  const sauceObject = req.file //Test si nouvelle image ou pas
+    ? //1ier cas: nouvelle image on récupère son URL
+      {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : //2ieme cas: pas de nouvelle image: copie du body
+      { ...req.body };
+  Sauce.updateOne(
+    { _id: req.params.id },
+    { ...sauceObject, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+//--------------------------------------------------------------
+//Suppression d'un objet = DELETE
+
+//DELETE avec suppression du dossier image
+// on vérifie si il y a une image à supprimer du dossier image
+// et on la supprimer avec unlink
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+//--------------------------------------------------------------
+//Extraction de tous les objets. GET
+exports.getAllStuff = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => {
+      res.status(200).json(sauces);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
