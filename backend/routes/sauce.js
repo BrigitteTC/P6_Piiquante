0ca1d3b593e22832b44d3ea243ffc8c6e9f5ce64
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-03-30 17:48:59 +0200

    routes/sauce.js: gelAllsauce remplacé par getAllSauce.

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -20,7 +20,7 @@ const multer = require("../middleware/multer-config");
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 
-router.get("/", auth, sauceCtrl.getAllsauce);
+router.get("/", auth, sauceCtrl.getAllSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
