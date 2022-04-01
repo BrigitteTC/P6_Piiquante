2b8ea1e071a29719a6288fa405a61b8b29edeb99
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-01 14:47:34 +0200

    controllers/llike.js: ajout UpdateSauceLikeTab et getIdFromUrl

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -14,12 +14,67 @@ const Like = require("../models/Like");
 
 const fs = require("fs"); //acces à la gestion des fichiers de Node
 
+/*-----------------------------------------------------------------------
+function: UpdateSauceLikeTab
+
+Objet: Mise à jour 
+    du nombre de like et dislike 
+    et des tableaus des userId like
+    et dislike de la sauce
+
+Parametres:
+  entrée: 
+    userId: UserId du liker
+    sauceId: Id de la sauce
+    likeValue:  valeur du like: 
+      1  0 et -1
+
+Algo:
+-----------------------------------------------------------------------*/
+function UpdatesauceLikeTab(userId, sauceId, likeValue) {
+  try {
+  } catch (e) {
+    console.log("UpdateSauceLikeTab " + e);
+  }
+}
+
+/*-------------------------------------------------------------------------
+function:  getIdFromUrl
+
+Objet: récupère le Id à partir de l'URL
+
+Paramètres:
+  Entrée: Url
+  Sortie: userId
+
+Algo:
+Url de la forme: /api/sauces/:id/like
+ou
+/api/sauces/:id
+
+------------------------------------------------------------------*/
+function getIdFromUrl(url) {
+  var userId = 0;
+  try {
+  } catch (e) {
+    console.log("getUserIdFromUrl " + e);
+  }
+
+  return userId;
+}
+
 /*-----------------------------------------------------------------------------------
 Fonction: createLike
 
 Objet: gestion des like et dislike
 
 verbe: POST
+
+Algo:
+  Acquisition de la requete like
+  Pour chaque like:
+    Récupération du userId
+    Modif de la sauce pour mise à jour params des like ou dislike
 
 -------------------------------------------------------------------------------*/
 
@@ -29,7 +84,7 @@ exports.createLike = (req, res, next) => {
     ...req.body,
   });
 
-  const useId = like.id; //UserId qui a like la sauce
+  const userId = like.id; //UserId qui a like la sauce
   const likeNum = like.like; //compteur de like ou dislike
 
   if (Number(likeNum) == 0) {
