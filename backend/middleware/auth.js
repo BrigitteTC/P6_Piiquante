1d139420c47a9788184f9382a853c6523f0df63f
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-01 18:45:29 +0200

    middleware/auth.js: ajout variable d'environnement secretKey

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,6 +8,10 @@ auteur: BTC
 Gestion authentification des utilisateurs
 pour protéger les routes sensibles
 ------------------------------------------------*/
+
+//configure dotenv pour les variables d'environnement
+require("dotenv").config();
+
 //jsonwebtoken pour vérifier les token
 
 const jwt = require("jsonwebtoken");
@@ -16,9 +20,10 @@ module.exports = (req, res, next) => {
   try {
     //on récupère le token dans le header = 2ieme elt du header apres le bearer
     const token = req.headers.authorization.split(" ")[1];
+    const secretKey = process.env.SECRET_KEY;
 
     // on décode le token avec verify et clé secrete
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, secretKey);
     const userId = decodedToken.userId;
 
     // verif userId de la requete correspond à celui du token
