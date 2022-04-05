52a430715f88f12471b1f7f48ac177716761c309
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-05 10:31:44 +0200

    controllers/sauce.js modifySauce: ajout test sur auth.user pour authentification user.

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -78,26 +78,38 @@ remarque:
     de tous les éléments de req.body . 
 -------------------------------------------------------------------------------*/
 exports.modifySauce = (req, res, next) => {
-  if (req.file) {
-    delFile(req.params.id); //On supprime l'ancien fichier de l'image
-  }
-  const sauceObject = req.file //Test si nouvelle image ou pas
-    ? //1ier cas: nouvelle image on récupère son URL
-
-      {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    if (!sauce) {
+      res.status(404).json({
+        error: new Error("sauce non trouvée"),
+      });
+    } else if (sauce.userId !== req.auth.userId) {
+      res.status(403).json({
+        error: new Error("403: unauthorized request"),
+      });
+    } else {
+      if (req.file) {
+        delFile(req.params.id); //On supprime l'ancien fichier de l'image
       }
-    : //2ieme cas: pas de nouvelle image: copie du body
-      { ...req.body };
-  Sauce.updateOne(
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
-  )
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
+      const sauceObject = req.file //Test si nouvelle image ou pas
+        ? //1ier cas: nouvelle image on récupère son URL
+
+          {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
+              req.file.filename
+            }`,
+          }
+        : //2ieme cas: pas de nouvelle image: copie du body
+          { ...req.body };
+      Sauce.updateOne(
+        { _id: req.params.id },
+        { ...sauceObject, _id: req.params.id }
+      )
+        .then(() => res.status(200).json({ message: "Objet modifié !" }))
+        .catch((error) => res.status(400).json({ error }));
+    }
+  });
 };
 
 /*------------------------------------------------------------
