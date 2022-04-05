259defed50e20ba3ef4c34825bf3a1a9a0ca9a02
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-05 11:18:19 +0200

    server.js: ajout try/catch

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,21 @@ const app = require("./app");
 //fonction normalizePort renvoie un port valide, qu'il soit fourni sous
 //la forme d'un numéro ou d'une chaîne;
 const normalizePort = (val) => {
-  const port = parseInt(val, 10);
+  try {
+    const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    return val;
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  } catch {
+    res.status(500).json({
+      error: new Error("Erreur server"),
+    });
   }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
 };
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
@@ -35,23 +41,29 @@ app.set("port", port);
 //et les gère de manière appropriée.Elle est ensuite enregistrée dans le serveur;
 
 const errorHandler = (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const address = server.address();
-  const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges.");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use.");
-      process.exit(1);
-      break;
-    default:
+  try {
+    if (error.syscall !== "listen") {
       throw error;
+    }
+    const address = server.address();
+    const bind =
+      typeof address === "string" ? "pipe " + address : "port: " + port;
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges.");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use.");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  } catch {
+    res.status(500).json({
+      error: new Error("Erreur server"),
+    });
   }
 };
 
