4af639cdefeed6e347e2174d71882907a392604a
Brigitte Tricot-Censier <brigittetc.pub@hotmail.fr>
2022-04-05 10:54:58 +0200

    controllers/user.js: ajout try/catch sur toutes les ft.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -31,21 +31,27 @@ require("dotenv").config();
    Sauvegarde utilisateur crée
 ------------------------------------------------------------*/
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
+  try {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash,
+        });
+        user
+          .save()
+          .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+          .catch((error) => res.status(401).json({ error }));
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
       });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(401).json({ error }));
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
+  } catch {
+    res.status(500).json({
+      error: new Error("Erreur server"),
     });
+  }
 };
 
 /*----------------------------------------------------------------------------
@@ -61,36 +67,42 @@ Algo:
 
 ----------------------------------------------------------------*/
 exports.login = (req, res, next) => {
-  const secretKey = process.env.SECRET_KEY;
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "utilisateur inconnu" });
-      }
+  try {
+    const secretKey = process.env.SECRET_KEY;
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({ error: "utilisateur inconnu" });
+        }
 
-      //bcrypt pour vérifier le mot de passe envoyé par l'utilisateur avec le hash enregistré
-      //Si correct renvoi du TOKEN au frontend
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect" });
-          }
-          res.status(200).json({
-            userId: user._id,
-            //token signé avec clé secrete et qui expire dans 24h avec chaine alleatoire
-            /*token: "TOKEN",*/
+        //bcrypt pour vérifier le mot de passe envoyé par l'utilisateur avec le hash enregistré
+        //Si correct renvoi du TOKEN au frontend
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              return res.status(401).json({ error: "Mot de passe incorrect" });
+            }
+            res.status(200).json({
+              userId: user._id,
+              //token signé avec clé secrete et qui expire dans 24h avec chaine alleatoire
+              /*token: "TOKEN",*/
 
-            token: jwt.sign(
-              {
-                userId: user._id,
-              },
-              secretKey,
-              { expiresIn: "24h" }
-            ),
-          });
-        })
-        .catch((error) => res.status(500).json({ error })); //500 = error serveur
-    })
-    .catch((error) => res.status(500).json({ error })); //500 = error serveur
+              token: jwt.sign(
+                {
+                  userId: user._id,
+                },
+                secretKey,
+                { expiresIn: "24h" }
+              ),
+            });
+          })
+          .catch((error) => res.status(500).json({ error })); //500 = error serveur
+      })
+      .catch((error) => res.status(500).json({ error })); //500 = error serveur
+  } catch {
+    res.status(500).json({
+      error: new Error("Erreur server"),
+    });
+  }
 };
